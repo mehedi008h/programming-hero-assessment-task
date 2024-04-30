f48e31101b08347c65a8d5d9aaa90f2524165e5d
mehedi008h <mehedi08h@gmail.com>
2024-04-30 12:47:10 +0600

    add Carousel in auth layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,3 +1,4 @@
+import { Carousel } from "antd";
 import React from "react";
 
 interface Props {
@@ -6,9 +7,21 @@ interface Props {
 
 const AuthLayout = ({ children }: Props) => {
     return (
-        <div className="w-full min-h-screen grid grid-cols-12">
+        <div className="w-full min-h-screen grid grid-cols-12 bg-neutral-100">
             <div className="col-span-5">{children}</div>
-            <div className="col-span-7">2</div>
+            <div className="w-full min-h-screen col-span-7 p-5">
+                <Carousel autoplay>
+                    <div className="bg-green-500 h-[calc(100vh-40px)] rounded-md">
+                        <h3>1</h3>
+                    </div>
+                    <div className="bg-blue-500 h-[calc(100vh-40px)] rounded-md">
+                        <h3>2</h3>
+                    </div>
+                    <div className="bg-pink-500 h-[calc(100vh-40px)] rounded-md">
+                        <h3>3</h3>
+                    </div>
+                </Carousel>
+            </div>
         </div>
     );
 };
